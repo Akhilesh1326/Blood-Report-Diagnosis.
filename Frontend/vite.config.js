5edd1023b8cc637930669de0faa94f460d8e876f
Akhilesh26 <akhileshpimple3@gmail.com>
2025-01-26 18:13:00 +0530

    modified vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy: {
-      "/user":"https://blood-report-diagnosis.onrender.com",
+      "/user":"http://localhost:3000",
     },
   },
   plugins: [react()],
